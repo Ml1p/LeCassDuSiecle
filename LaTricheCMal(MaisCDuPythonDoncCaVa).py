ba470f1b9904cdeb7e76f920e17840c8c99acc86
moaF <MoaF>
2024-11-28 15:34:39 +0100

    C BON ?

--- LaTricheCMal(MaisCDuPythonDoncCaVa).py
+++ LaTricheCMal(MaisCDuPythonDoncCaVa).py
@@ -1,14 +1,25 @@
 from sympy import *
 import numpy as np
 
+
+# Modifiez que les trucs avant la logique (Les Champs de vecteurs, les points, les fonctions...)
+
+
+
 #-----------------------------------------
 # EXO 1
 #-----------------------------------------
 x,y,z,t=symbols("x y z t")
 
-A=[1,1,3]
-B=[7,5,1]
-V=[-2*y,4*z,-2*x]
+
+A=[5,2,3]   # Point A (séparez les coordonées par des virgules comme dans l'exemple)
+B=[2,1,5]   # Point B
+V=[4*y,-2*z,-x]     # Champs de Vecteur V (Coordonnées séparées par des virgules)
+# [Vx , Vy ]
+
+# Pas touche après ça
+#-----------------------------------------
+
 
 C=[A[0]+t*(B[0]-A[0]),A[1]+t*(B[1]-A[1]),A[2]+t*(B[2]-A[2])]
 dl=[diff(C[0],t),diff(C[1],t),diff(C[2],t)]
@@ -30,27 +41,29 @@ print("\nQuestion 1 : \n\nI = "+str(integrate(Vdl,(t,0,1))))
 rho,phi=symbols("rho phi")
 
 
-A=[4,8*pi/4]
-B=[4,5*pi/4]
-V=[2*rho*(1+4*phi/pi),-2*rho*(1-4*phi/pi)]
-R=4
+V=[-5*rho*(1-3*phi/pi),rho*(1+2*phi/pi)]
 
+R=4            # Valeur de R Donée dans la première partie de l'exo
+A=[R,2*pi/4]
+B=[R,pi/4]
+
+# Partie 2
+
+phi0=2*pi/4 # Le phi qu'ils vous donnent dans la seconde partie de l'exo
+A2=[6,phi0]
+B2=[1,phi0]
+
+
+# Pshhit
+#-----------------------------------------
 
 V[1]=V[1]*rho
 V[1]=V[1].subs(rho,R)
 print("\nQuestion 2-1 :\n\nI = "+str(integrate(V[1],(phi,A[1],B[1]))/pi))
 
-A=[7,8*pi/4]
-B=[2,8*pi/4]
-phi0=8*pi/4
-
-
-#------------------------- Partie 2
-
-
 V[0]=V[0].subs(phi,phi0)
 
-print("\nQuestion 2-2 :\n\nI = "+str(integrate(V[0],(rho,A[0],B[0]))))
+print("\nQuestion 2-2 :\n\nI = "+str(integrate(V[0],(rho,A2[0],B2[0]))))
 
 #-----------------------------------------
 # EXO 3
@@ -58,13 +71,15 @@ print("\nQuestion 2-2 :\n\nI = "+str(integrate(V[0],(rho,A[0],B[0]))))
 u=symbols("u")
 
 
-V=[u/(-3*x+8*y)**2,-16/(-3*x+8*y)**2]
+V=[u/(-7*x-8*y)**2,16/(-7*x-8*y)**2]  # Champs de vecteur (notez u, u simplement)
+                                    # Et ² c'est **2
 
 
+# Retirez vos mains après cette partie
+#-----------------------------------------
 g=V[1]-diff(integrate(V[0],x),y)
 u0=solve(g,u)[0]
 print("\nQuestion 3-1: \nu = "+str(solve(g,u)[0]))
-
 
 A=[1,0]
 B=[0,1]
@@ -86,10 +101,12 @@ print("\nQuestion 3-2 : \n\nI = "+str(I))
 # EXO 4
 #-----------------------------------------
 
+f=-2*x**2-16*x*y+2*y*z         # Fonction qu'ils vous donnent
+V=[-2*x+6*y,2*y-5*z,2*z+4*x]
 
-f=2*x**2+16*x*y-y*z
-V=[2*x-y,-4*y+3*z,-z+3*x]
 
+# Toujours pareil, après ça c'est le cerveau
+#-----------------------------------------
 
 grad=[diff(f,x),diff(f,y),diff(f,z)]
 eq1=Eq(grad[0],0)
@@ -109,24 +126,32 @@ print("\nQuestion 4-3:\na = "+str(a)+"\tb = "+str(b)+"\tc = "+str(c))
 # EXO 5
 #-----------------------------------------
 
-f=abs(x-y)
-bx=[0,7]
-by=[0,4]
+bx=[0,7] # Bornes de l'intégrale sur x, [borne négative, borne positive]
+by=[0,4] # Bornes de l'intégrale sur y
 
+
+# Approchez à vos risques et périls
+#-----------------------------------------
+
+f=abs(x-y)
 print("\nQuestion 5:\n"+str(integrate(f,(x,bx[0],bx[1]),(y,by[0],by[1]))))
+
 
 #-----------------------------------------
 # EXO 6
 #-----------------------------------------
 a,b,c=symbols("a b c")
 
+A=[5,3,6]
+B=[6,3,6]
+C=[5,7,6]
+D=[5,3,9]
+c0=4        # La valeur de c qu'ils vous donnent dans l'énoncé
+f=y-3       # La fonction qu'il faut intégrer
 
-A=[2,5,3]
-B=[6,5,3]
-C=[2,10,3]
-D=[2,5,7]
-c0=20
-f=y-5
+
+# Bas les pattes
+#-----------------------------------------
 
 eq=Eq(a*(x-D[0])+b*(y-D[1])+c*(z-D[2]),0)
 
@@ -152,10 +177,9 @@ v=min(A[1],B[1],C[1],D[1])
 w=min(A[0],B[0],C[0],D[0])
 print("\nQuestion 6-2:\nt = "+str(t0)+"\tu = "+str(u0)+"\tv = "+str(v)+"\tw = "+str(w))
 
-
 fTrident=solve(a0*(x-D[0])+b0*(y-D[1])+c0*(z-D[2]),x)
 
-print("Trident: "+str(fTrident))
+print("\nTrident (ou psi comme vous préférez): "+str(fTrident)+" (C'est pas factorisé comme ils le demandent mais vous pouvez le copier coller et ça passe crême)")
 
 fphi=solve(b0*(y-D[1])+c0*(z-D[2]),y)
 
@@ -178,11 +202,14 @@ print("\nQuestion 6-3:\nI = "+str(integrate(f,(x,w,fTrident),(y,v,fphi),(z,t0,u0
 #-----------------------------------------
 r,Teta=symbols("r Teta")
 
+V=[-5*r*cos(Teta),4*r*sin(Teta)]
 
-f=(x-2)**2+y**2-4
-V=[2*r*cos(Teta),-2*r*sin(Teta)]
-coefx=2
-r0=9
+coefx=5     # C'est le 2 dans (x-2)² ou le 5 dans (x-5)² (et il est positif c'est pas -5)
+r0=11       # Le r constant qu'ils vous donnent
+
+
+# Fuyez pauvres fou
+#-----------------------------------------
 
 b0=Rational(coefx*2,r0)
 
@@ -193,15 +220,6 @@ V[0]=V[0].subs(r,r0)
 
 I=V[0].subs(sin(Teta),u)
 I=I/cos(Teta)
-print("Uniquement le commencement "+str(I))
-I=integrate(I,(u,pi/2,sin(cos(phi))),(phi,-pi/2,pi/2)) #Franchement pas sur des bornes sur u (sin(cos(phi)) ça résoud pas le pb)
-print("La fin ? "+str(I))
-
-
-I=integrate(V[0],(Teta,0,b0*cos(phi)))
-I=I.subs(cos(phi),u)
-I=I/-sin(phi)
-I=integrate(I,(u,-1,1),(phi,-pi/2,pi/2))
-
+I=integrate(I,(u,0,b0*cos(phi)),(phi,-pi/2,pi/2))/pi
 
 print("\nQuestion 7-2:\nI = "+str(I))
